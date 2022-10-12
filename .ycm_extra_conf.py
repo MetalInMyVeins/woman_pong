d1a952415333b2cdcdfe50446f79baeaf6f1fb7c
MetalInMyVeins <hossainmdfahim66@gmail.com>
2022-10-12 14:09:12 +0600

    added .ycm_extra_conf.py

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,4 @@
+def Settings(**kwargs):
+    return{
+            'flags':['-x', 'c++', '-std=c++20', '-g', '-Wall', '-Wextra', '-Wpedantic', '-Werror', '-Wno-unused-variable', '-Wno-unused-parameter', '-Wno-unused-private-field', '-I/home/metal888/cppu/gpic/ch1/woman_pong/include'],
+            }
